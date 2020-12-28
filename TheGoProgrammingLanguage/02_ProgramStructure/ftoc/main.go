3662f73546543bfde245c1f4f0a8a871bcf84ad9
Krishana Pal Singh Chouhan <kpsing.chouhan@gmail.com>
2020-12-28 10:18:28 +0000

    added ftoc program

--- TheGoProgrammingLanguage/02_ProgramStructure/ftoc/main.go
+++ TheGoProgrammingLanguage/02_ProgramStructure/ftoc/main.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func main() {
 	const freeingzF, boilingF = 32.0, 212.0
-	fmt.Printf("%g° F = %g° C\n", freeingzF, fToC(freeingzF)) // "32° F = 0° C"
-	fmt.Printf("%g° F = %g° C\n", boilingF, fToC(boilingF))   // "212° F = 100° C"
+	fmt.Printf("%g degree F = %g degree C\n", freeingzF, fToC(freeingzF)) // "32 F = 0 C"
+	fmt.Printf("%g degree F = %g degree C\n", boilingF, fToC(boilingF))   // "212 F = 100 C"
 }
 
 func fToC(f float64) float64 {
